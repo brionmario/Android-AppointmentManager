13a6b49b48634ab9aaa27394b1acd2eb214bdf81
Brion Mario <brion.2015283@iit.ac.lk>
2017-04-07 08:06:17 +0530

    Fixed the put extra issue

--- app/src/main/java/com/apareciumlabs/brionsilva/madscheduler/CreateAppointmentScreen.java
+++ app/src/main/java/com/apareciumlabs/brionsilva/madscheduler/CreateAppointmentScreen.java
@@ -1,7 +1,9 @@
 package com.apareciumlabs.brionsilva.madscheduler;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class CreateAppointmentScreen extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class CreateAppointmentScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_appointment_screen);
+
+        Intent intent = getIntent();
+
+        Toast.makeText(getBaseContext(),intent.getStringExtra("Date"),Toast.LENGTH_SHORT ).show();
     }
 }
--- app/src/main/java/com/apareciumlabs/brionsilva/madscheduler/HomeScreen.java
+++ app/src/main/java/com/apareciumlabs/brionsilva/madscheduler/HomeScreen.java
@@ -1,14 +1,19 @@
 package com.apareciumlabs.brionsilva.madscheduler;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class HomeScreen extends AppCompatActivity implements View.OnClickListener {
 
     private Button crtAppoBtn,editAppoBtn,delAppoBtn,moveAppoBtn,searchBtn;
+    CalendarView calendarView;
+    private int getYear,getMonth,getDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +35,19 @@ public class HomeScreen extends AppCompatActivity implements View.OnClickListene
 
         searchBtn = (Button) findViewById(R.id.searchButton);
         searchBtn.setOnClickListener(this);
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+
+                getYear = year ;
+                getMonth = month;
+                getDay = dayOfMonth ;
+               // Toast.makeText(getBaseContext() , year + "/" + month + "/" + dayOfMonth , Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     @Override
@@ -38,7 +56,11 @@ public class HomeScreen extends AppCompatActivity implements View.OnClickListene
         switch (v.getId()) {
             case R.id.createAppointmentButton :{
 
+                //get the date frm the calender view and store it in a variable
+                long date = calendarView.getDate();
+
                 Intent intent = new Intent(getBaseContext() , CreateAppointmentScreen.class);
+                intent.putExtra("Date" , getYear + "/" + getMonth + "/" + getDay);
                 startActivity(intent);
 
             }
