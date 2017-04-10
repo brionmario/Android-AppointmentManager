0cc60eafe314127aafbccdc9e2ef78403f9e5dc7
Brion Mario <brion.2015283@iit.ac.lk>
2017-04-11 03:50:47 +0530

    Finished the search functionality

--- app/src/main/java/com/apareciumlabs/brionsilva/madscheduler/Appointment.java
+++ app/src/main/java/com/apareciumlabs/brionsilva/madscheduler/Appointment.java
@@ -34,10 +34,6 @@ public class Appointment {
         return date;
     }
 
-    public void setDate(String date) {
-        this.date = date;
-    }
-
     public String getTime() {
         return time;
     }
@@ -50,15 +46,8 @@ public class Appointment {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getDetails() {
         return details;
     }
 
-    public void setDetails(String details) {
-        this.details = details;
-    }
 }
